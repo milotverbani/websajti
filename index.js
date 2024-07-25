5a68cc0dc1c6842d885f8d4bff327568e6653794
milotverbani <milot.verbani233@gmail.com>
2024-07-25 20:29:13 +0200

    foto edeh alerti

--- index.js
+++ index.js
@@ -100,3 +100,24 @@ window.addEventListener("scroll", function() {
         containerObserver.observe(container);
     });
 });
+
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Mesazhi u dergua me sukses', 'success')
+  })
+}
